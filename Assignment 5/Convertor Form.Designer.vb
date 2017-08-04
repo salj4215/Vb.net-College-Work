c5b211dfa9accec161002b05d670d0bdaf3731c2
Salleh Jahaf <salj4215@gmail.com>
2017-08-03 22:58:13 -0400

    Add files via upload

---
+++ Assignment 5/Convertor Form.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblConvertedWord = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.txtUserWord = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(43, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(176, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter &word you want to convert:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(353, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(191, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Your word converted to pig latin is:"
+        '
+        'lblConvertedWord
+        '
+        Me.lblConvertedWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblConvertedWord.Location = New System.Drawing.Point(356, 57)
+        Me.lblConvertedWord.Name = "lblConvertedWord"
+        Me.lblConvertedWord.Size = New System.Drawing.Size(189, 25)
+        Me.lblConvertedWord.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(248, 59)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(75, 23)
+        Me.btnConvert.TabIndex = 2
+        Me.btnConvert.Text = "&Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(470, 132)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'txtUserWord
+        '
+        Me.txtUserWord.Location = New System.Drawing.Point(46, 60)
+        Me.txtUserWord.Name = "txtUserWord"
+        Me.txtUserWord.Size = New System.Drawing.Size(173, 23)
+        Me.txtUserWord.TabIndex = 1
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(596, 194)
+        Me.Controls.Add(Me.txtUserWord)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblConvertedWord)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmMain"
+        Me.Text = "English To Pig Latin Convertor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lblConvertedWord As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents txtUserWord As System.Windows.Forms.TextBox
+
+End Class
---
+++ Assignment 5/Convertor Form.vb
@@ -0,0 +1,91 @@
+﻿'Pig Latin Conversion Program (assignment 5)
+'This program converts words entered by user to pig latin
+'Created by Salleh Jahaf
+'Version 1.02 last updated 11/24/2015
+
+Public Class frmMain
+
+    'Code to exit program when user clicks exit button
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+    'Code to display word converted to pig latin
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+
+        Dim strUserWord As String
+        Dim strConvertedWord As String
+
+        'if no word is entered in the text box, show message to user and give text box focus
+        If txtUserWord.Text = String.Empty Then
+            MessageBox.Show("Please enter a word into the text box.", "No Word Entered", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            txtUserWord.Focus()
+            Exit Sub
+        End If
+
+        'assign user word to variable after trimming spaces and making all letters upper case
+        strUserWord = txtUserWord.Text.Trim
+        strUserWord = strUserWord.ToUpper
+
+        'give variable that will hold converted word any value
+        strConvertedWord = strUserWord
+
+        'Call sub procedure to convert user word to converted word
+        Call Convert_to_Pig_Latin(strUserWord, strConvertedWord)
+
+        'Display converted word
+        lblConvertedWord.Text = strConvertedWord
+
+
+
+    End Sub
+
+    'Code to convert user word to pig latin
+    Private Sub Convert_to_Pig_Latin(ByVal WordToConvert As String, ByRef PigLatinWord As String)
+
+        Dim intNumChars As Integer
+        Dim strChar As String
+
+        'count how many letters users word consists of and assign to variable
+        intNumChars = WordToConvert.Length
+
+        'if user word begins with a vowel or Y, add "-way" to the end of user's word and exit procedure
+        If WordToConvert Like "[AEIOUY]*" Then
+            PigLatinWord = PigLatinWord.Insert(intNumChars, "-WAY")
+            Exit Sub
+        End If
+
+        'If word contains a vowel or "Y", add a dash to the end of the word then send consonants at the beginning of the word to the end until a vowel or "Y" is at the beginning, then add "ay" to the end
+        If WordToConvert.Contains("A") OrElse WordToConvert.Contains("E") OrElse WordToConvert.Contains("I") OrElse WordToConvert.Contains("O") OrElse WordToConvert.Contains("U") OrElse WordToConvert.Contains("Y") Then
+            PigLatinWord = PigLatinWord.Insert(intNumChars, "-")
+            For intIndex As Integer = 0 To intNumChars - 1
+                strChar = WordToConvert.Substring(intIndex, 1)
+                If strChar Like "[!AEIOUY]" Then
+                    PigLatinWord = PigLatinWord.Remove(0, 1)
+                    PigLatinWord = PigLatinWord.Insert(intNumChars, strChar)
+                Else
+                    PigLatinWord = PigLatinWord & "AY"
+                    Exit For
+                End If
+            Next intIndex
+            Exit Sub
+        End If
+
+        'Add "-way" at the end if no vowels or "Y" is in user's word
+        PigLatinWord = PigLatinWord & "-WAY"
+
+    End Sub
+
+    'Code to select all text within text box when user tabs into field
+    Private Sub txtUserWord_Enter(sender As Object, e As EventArgs) Handles txtUserWord.Enter
+
+        txtUserWord.SelectAll()
+
+    End Sub
+
+    'Clear output when user types changes contents of text field
+    Private Sub txtUserWord_TextChanged(sender As Object, e As EventArgs) Handles txtUserWord.TextChanged
+
+        lblConvertedWord.Text = String.Empty
+
+    End Sub
+End Class
