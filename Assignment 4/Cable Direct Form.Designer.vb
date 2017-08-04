2c790ef559171a9ee08436c60b6b9a20de5b030f
Salleh Jahaf <salj4215@gmail.com>
2017-08-03 22:57:10 -0400

    Add files via upload

---
+++ Assignment 4/Cable Direct Form.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.radBusiness = New System.Windows.Forms.RadioButton()
+        Me.radResidential = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lstPremium = New System.Windows.Forms.ListBox()
+        Me.lstConnections = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblTotalDue = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'radBusiness
+        '
+        Me.radBusiness.AutoSize = True
+        Me.radBusiness.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radBusiness.Location = New System.Drawing.Point(12, 12)
+        Me.radBusiness.Name = "radBusiness"
+        Me.radBusiness.Size = New System.Drawing.Size(70, 19)
+        Me.radBusiness.TabIndex = 0
+        Me.radBusiness.Text = "&Business"
+        Me.radBusiness.UseVisualStyleBackColor = True
+        '
+        'radResidential
+        '
+        Me.radResidential.AutoSize = True
+        Me.radResidential.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.radResidential.Location = New System.Drawing.Point(12, 48)
+        Me.radResidential.Name = "radResidential"
+        Me.radResidential.Size = New System.Drawing.Size(82, 19)
+        Me.radResidential.TabIndex = 1
+        Me.radResidential.Text = "&Residential"
+        Me.radResidential.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(165, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "&Premium channels:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(310, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "&Connections:"
+        '
+        'lstPremium
+        '
+        Me.lstPremium.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstPremium.FormattingEnabled = True
+        Me.lstPremium.ItemHeight = 15
+        Me.lstPremium.Location = New System.Drawing.Point(168, 36)
+        Me.lstPremium.Name = "lstPremium"
+        Me.lstPremium.Size = New System.Drawing.Size(63, 94)
+        Me.lstPremium.TabIndex = 3
+        '
+        'lstConnections
+        '
+        Me.lstConnections.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstConnections.FormattingEnabled = True
+        Me.lstConnections.ItemHeight = 15
+        Me.lstConnections.Location = New System.Drawing.Point(313, 36)
+        Me.lstConnections.Name = "lstConnections"
+        Me.lstConnections.Size = New System.Drawing.Size(63, 94)
+        Me.lstConnections.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 195)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 15)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Total due:"
+        '
+        'lblTotalDue
+        '
+        Me.lblTotalDue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalDue.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotalDue.Location = New System.Drawing.Point(12, 220)
+        Me.lblTotalDue.Name = "lblTotalDue"
+        Me.lblTotalDue.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalDue.TabIndex = 7
+        Me.lblTotalDue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(157, 220)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(133, 23)
+        Me.btnCalculate.TabIndex = 8
+        Me.btnCalculate.Text = "Calculate Total &Due"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(349, 220)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(450, 262)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblTotalDue)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lstConnections)
+        Me.Controls.Add(Me.lstPremium)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.radResidential)
+        Me.Controls.Add(Me.radBusiness)
+        Me.Name = "frmMain"
+        Me.Text = "Cable Direct"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents radBusiness As System.Windows.Forms.RadioButton
+    Friend WithEvents radResidential As System.Windows.Forms.RadioButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents lstPremium As System.Windows.Forms.ListBox
+    Friend WithEvents lstConnections As System.Windows.Forms.ListBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents lblTotalDue As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Assignment 4/Cable Direct Form.vb
@@ -0,0 +1,159 @@
+﻿'Cable Direct Due Calculator (assignment 4)
+'This program displays a customer's total due owed to Cable Direct
+'Created by Salleh Jahaf
+'Version 1.01 last updated 11/04/2015
+
+Option Strict On
+Option Explicit On
+
+Public Class frmMain
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        'verify that the user wants to exit the application
+
+        Dim dlgButton As DialogResult
+
+        dlgButton = MessageBox.Show("Are you sure you want to exit?", "Cable Direct", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+
+        'if the No button was clicked, don't close the form
+
+        If dlgButton = Windows.Forms.DialogResult.No Then
+            e.Cancel = True
+        End If
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Display numbers 0 - 20 in the lstPremium listbox control
+
+        For intPremiumChannels As Integer = 0 To 20
+            lstPremium.Items.Add(intPremiumChannels.ToString("N0"))
+        Next intPremiumChannels
+
+        'Display numbers 0 - 100 in the lstConnections listbox control
+        For intConnections As Integer = 0 To 100
+            lstConnections.Items.Add(intConnections.ToString("N0"))
+        Next intConnections
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim intChannels As Integer
+        Dim intConnections As Integer
+        Dim dblTotalDue As Double
+
+        'Data validation
+
+        If Data_Validated_OK() = False Then Exit Sub
+
+        'assign variables to user selection
+
+        intChannels = Convert.ToInt32(lstPremium.SelectedItem.ToString)
+        intConnections = Convert.ToInt32(lstConnections.SelectedItem.ToString)
+
+        'If residential customer, calculate cable bill 
+
+        If radResidential.Checked Then
+            dblTotalDue = getResTotalDue(intChannels)
+        Else
+            dblTotalDue = getBusTotalDue(intChannels, intConnections)
+        End If
+
+        dblTotalDue = Math.Round(dblTotalDue, 2)
+
+        lblTotalDue.Text = dblTotalDue.ToString("C2")
+
+    End Sub
+
+    'Data Validation function
+    Private Function Data_Validated_OK() As Boolean
+
+        'make sure user selected customer type
+
+        If radBusiness.Checked = False And radResidential.Checked = False Then
+            MessageBox.Show("Please select what type of customer you are.", "Cable Direct", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return False
+        End If
+
+        'make sure user selects number of premium channels
+
+        If lstPremium.SelectedIndex = -1 Then
+            MessageBox.Show("Please choose how many channels you are subscribed to.", "Cable Direct", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            lstPremium.Focus()
+            Return False
+        End If
+
+        'make sure business customer selects number of connections
+
+        If radBusiness.Checked And lstConnections.SelectedIndex <= 0 Then
+            MessageBox.Show("Please choose how many connections you have. You must have at least one connection.", "Cable Direct", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return False
+        End If
+
+        Return True
+    End Function
+    Private Function getResTotalDue(intPremium As Integer) As Double
+        'Calculates and returns total due for residential customers
+
+        Const dblPROCESSING_FEE As Double = 4.5
+        Const intBASIC_SERVICE_FEE As Integer = 30
+        Const intPREMIUM_PER_CHANNEL As Integer = 5
+        Dim dblTotal As Double
+
+        dblTotal = intPremium * intPREMIUM_PER_CHANNEL + dblPROCESSING_FEE + intBASIC_SERVICE_FEE
+
+        If lstConnections.SelectedIndex > 0 Then
+            lstConnections.SelectedIndex = 0
+        End If
+
+        Return dblTotal
+
+    End Function
+    Private Function getBusTotalDue(intPrem As Integer, intConnects As Integer) As Double
+        'Calculates and returns total due for business customers
+
+        Const dblPROCESSING_FEE As Double = 16.5
+        Const intPREMIUM_PER_CHANNEL As Integer = 50
+        Const intADDITIONAL_CONNECT As Integer = 4
+        Dim intBasicServiceFee As Integer
+        Dim dblTotal As Double
+
+        'Any number of connections up to 10 have a flat fee of $80
+        If intConnects <= 10 Then
+            intBasicServiceFee = 80
+        Else
+            'Each connection over 10 connections are $4 each, on top of flat fee of $80
+            intBasicServiceFee = (intConnects - 10) * intADDITIONAL_CONNECT + 80
+        End If
+
+        dblTotal = intPrem * intPREMIUM_PER_CHANNEL + intBasicServiceFee + dblPROCESSING_FEE
+
+        Return dblTotal
+    End Function
+
+    Private Sub radResidential_CheckedChanged(sender As Object, e As EventArgs) Handles radResidential.CheckedChanged
+
+        'Set connections to 0 if residential customer
+        'Have customer select number of premium channels
+
+        If radResidential.Checked Then
+            lstConnections.SelectedIndex = 0
+            lstPremium.SelectedIndex = -1
+            'Have user select number of connections and premium channels if business customer
+        Else
+            lstConnections.SelectedIndex = -1
+            lstPremium.SelectedIndex = -1
+        End If
+
+    End Sub
+    'Clear labels if user changes input
+    Private Sub ClearLabel(sender As Object, e As EventArgs) Handles lstPremium.SelectedValueChanged, lstConnections.SelectedValueChanged, radBusiness.CheckedChanged,
+        radResidential.CheckedChanged
+
+        lblTotalDue.Text = String.Empty
+
+    End Sub
+End Class
